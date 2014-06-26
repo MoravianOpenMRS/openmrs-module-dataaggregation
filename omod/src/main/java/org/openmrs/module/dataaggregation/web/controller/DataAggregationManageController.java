5bedbab6ea9d5cc3ef3ffbe3d9dd780fb5766cf2
openMRS <openMRS@slytherin.cs.moravian.edu>
2014-06-26 14:27:49 -0400

    Added new Queries link to header and more parameters

--- omod/src/main/java/org/openmrs/module/dataaggregation/web/controller/DataAggregationManageController.java
+++ omod/src/main/java/org/openmrs/module/dataaggregation/web/controller/DataAggregationManageController.java
@@ -59,9 +59,9 @@ public class  DataAggregationManageController {
 		LinkedList<String> testsOrdered = new LinkedList<String>();
 		testsOrdered.add("X-RAY, CHEST");
 		testsOrdered.add("CD4 PANEL");		
-		model.addAttribute("testsOrdered", service.getTestsOrdered(testsOrdered, "1900-01-20 00:00:00", "2100-01-20 00:00:00"));
+		model.addAttribute("testsOrdered", service.getTestsOrdered(testsOrdered, "1900-01-20 00:00:00", "2100-01-20 00:00:00", -1, -1));
 		
-		model.addAttribute("weights", service.getWeights());
+		model.addAttribute("weights", service.getWeights('M', 20, 30));
 
 	}
 	
