08a229edf9e7c7ab04eb76de9abfcb88ac2a275c
openmrs <openmrs@buckbeak.cs.moravian.edu>
2014-07-09 13:52:29 -0400

    Fixed interface for CSVFormatter

--- api/src/test/java/org/openmrs/module/dataaggregation/api/CSVFormatter.java
+++ api/src/test/java/org/openmrs/module/dataaggregation/api/CSVFormatter.java
@@ -7,7 +7,7 @@ import org.hibernate.Session;
 
 public class CSVFormatter {
 
-	public static String testQueryFormatter(List<Object> results){
+	public static String formatDiseaseBurden(List<Object> results){
 	
 		StringBuilder resultString = new StringBuilder();
     	resultString.append("testName:count\n");
@@ -24,7 +24,7 @@ public class CSVFormatter {
     	return resultString.toString();
     }
 	
-	public static String diseaseQueryFormatter(List<Object> results){
+	public static String formatTestsOrdered(List<Object> results){
 		
 		StringBuilder resultString = new StringBuilder();
     	resultString.append("diseaseName:count\n");
@@ -32,16 +32,16 @@ public class CSVFormatter {
 		for (Object o : results) {
 			// Cast each object into an array where each column is another index into the array
 			Object[] vals = (Object[]) o;
-			// vals[1] is the name of the disease
-			// vals[2] is the count for the disease
-			// vals[0] is just the concept_id of the disease which we may or may not need but that is why vals[0] is not used here
+			// vals[1] is the name of the test
+			// vals[2] is the count for the test
+			// vals[0] is just the concept_id of the test which we may or may not need but that is why vals[0] is not used here
 			resultString.append(vals[1] + ":" + vals[2] + "\n");
 		}
 		
     	return resultString.toString();
 	}
 	
-	public static String weightQueryFormatter(List<Object> results){
+	public static String formatWeights(List<Object> results){
 		
 		StringBuilder resultString = new StringBuilder();
     	resultString.append("personId:personWeight(KG)\n");
@@ -60,4 +60,6 @@ public class CSVFormatter {
 		
     	return resultString.toString();
 	}
+	
+	private static String format
 }
