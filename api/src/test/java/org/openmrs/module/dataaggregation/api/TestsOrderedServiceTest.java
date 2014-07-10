3513e1809ad4afb8692dcac03854a94e37c70bff
openmrs <openmrs@lupin.cs.moravian.edu>
2014-07-10 10:28:48 -0400

    Finished testing TestsOrdered

---
+++ api/src/test/java/org/openmrs/module/dataaggregation/api/TestsOrderedServiceTest.java
@@ -0,0 +1,615 @@
+package org.openmrs.module.dataaggregation.api;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.dataaggregation.api.impl.DataCounts;
+import org.openmrs.test.BaseModuleContextSensitiveTest;
+import org.openmrs.test.SkipBaseSetup;
+
+public class TestsOrderedServiceTest extends BaseModuleContextSensitiveTest {
+	
+private static final String DATASET_XML_PATH_NAME = "DataAggregationDiseaseBurdenTestDataset.xml";
+	
+	private DataAggregationService service;
+	
+	//
+	@Before
+	public void setup() throws Exception {
+		executeDataSet(DATASET_XML_PATH_NAME);
+		service = Context.getService(DataAggregationService.class);
+	}
+	
+	/*
+	@Test
+	@SkipBaseSetup
+	public void testTests() {	
+		
+		System.out.println("Running Test: " + Thread.currentThread().getStackTrace()[1].getMethodName());			
+		
+		for(int count = 0; count < 50; count++)
+		{
+			// testing to see if all the null will default to everything
+			System.out.println("Count = " + count);
+			service.getTestsOrdered(null, null, null, null, null, null);
+		}
+		
+		System.out.println("Done!");
+	}
+	*/
+	
+	
+	@Test
+	@SkipBaseSetup
+	public void testAllParametersNull() {	
+		
+		System.out.println("Running Test: " + Thread.currentThread().getStackTrace()[1].getMethodName());
+		
+		// testing to see if all the null will default to everything
+
+		List<Object> results = service.getTestsOrdered(null, null, null, null, null, null);		
+		
+		DataCounts map = new DataCounts(results);
+				
+		assertEquals("1", map.getValue("HEMOGLOBIN"));		
+		assertEquals("2", map.getValue("OTHER NON-CODED"));	
+		assertEquals("3", map.getValue("CD4 PANEL"));
+		assertEquals("4", map.getValue("X-RAY CHEST"));
+		assertEquals("5", map.getValue("COMPLETE BLOOD COUNT"));
+	
+	}
+	
+	@Test
+	@SkipBaseSetup
+	public void testTestParameterNull() {	
+		
+		System.out.println("Running Test: " + Thread.currentThread().getStackTrace()[1].getMethodName());
+		
+		// testing to see if just putting null into the Test list will make it get all the Tests
+		
+		String cityList = "Indianapolis:Philadelphia:New York:Chicago:Detroit:Los Angeles:San Diego";
+		
+		List<Object> results = service.getTestsOrdered(null, cityList, "0000-01-01", "2114-07-01", 1, 15000);
+		
+		DataCounts map = new DataCounts(results);
+				
+		assertEquals("1", map.getValue("HEMOGLOBIN"));		
+		assertEquals("2", map.getValue("OTHER NON-CODED"));	
+		assertEquals("3", map.getValue("CD4 PANEL"));
+		assertEquals("4", map.getValue("X-RAY CHEST"));
+		assertEquals("5", map.getValue("COMPLETE BLOOD COUNT"));
+	
+	}
+	
+	@Test
+	@SkipBaseSetup
+	public void testAllTests() {
+		
+		System.out.println("Running Test: " + Thread.currentThread().getStackTrace()[1].getMethodName());
+		
+		// testing that if all the Tests are in the list then it will get each one
+				
+		String tests = "HEMOGLOBIN:OTHER NON-CODED:CD4 PANEL:X-RAY CHEST:COMPLETE BLOOD COUNT";
+		
+		 
+		List<Object> results = service.getTestsOrdered(tests, null, null, null, -1, -1);
+		
+		DataCounts map = new DataCounts(results);
+				
+		assertEquals("1", map.getValue("HEMOGLOBIN"));		
+		assertEquals("2", map.getValue("OTHER NON-CODED"));	
+		assertEquals("3", map.getValue("CD4 PANEL"));
+		assertEquals("4", map.getValue("X-RAY CHEST"));
+		assertEquals("5", map.getValue("COMPLETE BLOOD COUNT"));
+		
+	}
+	
+	@Test
+	@SkipBaseSetup
+	public void testOneTest() {
+		
+		System.out.println("Running Test: " + Thread.currentThread().getStackTrace()[1].getMethodName());
+		
+		// testing to make sure that if one particular Test is wanted it will return just that Test
+		
+		String tests = "COMPLETE BLOOD COUNT";
+		
+		 
+		List<Object> results = service.getTestsOrdered(tests, null, null, null, -1, -1);
+		
+		DataCounts map = new DataCounts(results);
+				
+		assertEquals("5", map.getValue("COMPLETE BLOOD COUNT"));		
+		assertNull(map.getValue("HEMOGLOBIN"));
+	}
+	
+	@Test
+	@SkipBaseSetup
+	public void testCoupleOfTests() {
+		
+		System.out.println("Running Test: " + Thread.currentThread().getStackTrace()[1].getMethodName());
+		
+		// testing to make sure that it can also get a few Tests but not all 
+		
+		String Tests = "COMPLETE BLOOD COUNT:CD4 PANEL";
+		
+		 
+		List<Object> results = service.getTestsOrdered(Tests, null, null, null, -1, -1);
+		
+		DataCounts map = new DataCounts(results);
+				
+		assertEquals("5", map.getValue("COMPLETE BLOOD COUNT"));
+		assertEquals("3", map.getValue("CD4 PANEL"));
+		assertNull(map.getValue("HEMOGLOBIN"));
+	}
+	
+	@Test
+	@SkipBaseSetup
+	public void testTestNotExists() {
+		
+		System.out.println("Running Test: " + Thread.currentThread().getStackTrace()[1].getMethodName());
+		
+		// testing to make sure that if a Test is specified it will not return anything for that Test if there are no cases of it
+		
+		String Tests = "COOTIES";
+		
+		 
+		List<Object> results = service.getTestsOrdered(Tests, null, null, null, -1, -1);
+		
+		DataCounts map = new DataCounts(results);
+				
+		assertNull(map.getValue("COOTIES"));
+		assertNull(map.getValue("HEMOGLOBIN"));
+		
+	}
+	
+	@Test
+	@SkipBaseSetup
+	public void testOneTestNotExistsOtherDoes() {
+		
+		System.out.println("Running Test: " + Thread.currentThread().getStackTrace()[1].getMethodName());
+		
+		// testing that with a Test that does not exist it will still get the Test that does exist
+		
+		String Tests = "CD4 PANEL:COOTIES";
+		
+		 
+		List<Object> results = service.getTestsOrdered(Tests, null, null, null, -1, -1);
+		
+		DataCounts map = new DataCounts(results);
+				
+		assertEquals("3", map.getValue("CD4 PANEL"));
+		assertNull(map.getValue("COOTIES"));
+		assertNull(map.getValue("HEMOGLOBIN"));
+	}
+	
+	@Test
+	@SkipBaseSetup
+	public void testNeitherTestExists() {
+		
+		System.out.println("Running Test: " + Thread.currentThread().getStackTrace()[1].getMethodName());
+		
+		// testing that with multiple Tests it will not get a cases for either of them
+		
+		String Tests = "ROOSTER-POX:COOTIES";
+		
+		 
+		List<Object> results = service.getTestsOrdered(Tests, null, null, null, -1, -1);
+		
+		DataCounts map = new DataCounts(results);
+				
+		assertNull(map.getValue("ROOSTER-POX"));
+		assertNull(map.getValue("COOTIES"));
+		assertNull(map.getValue("HEMOGLOBIN"));
+	}
+	
+	@Test
+	@SkipBaseSetup
+	public void testMinNumberOfCases() {
+		
+		System.out.println("Running Test: " + Thread.currentThread().getStackTrace()[1].getMethodName());
+		
+		// testing that it will only get Tests with the certain cases above a threshold (inclusive at the threshold)
+		
+		
+		 
+		List<Object> results = service.getTestsOrdered(null, null, null, null, 3, -1);
+		
+		DataCounts map = new DataCounts(results);
+		
+		assertEquals("3", map.getValue("CD4 PANEL"));
+		assertEquals("4", map.getValue("X-RAY CHEST"));
+		assertEquals("5", map.getValue("COMPLETE BLOOD COUNT"));
+		assertNull(map.getValue("HEMOGLOBIN"));
+	}
+	
+	@Test
+	@SkipBaseSetup
+	public void testMaxNumberOfCases() {
+		
+		System.out.println("Running Test: " + Thread.currentThread().getStackTrace()[1].getMethodName());
+		
+		// testing that it will only get Tests with certain cases below a threshold (inclusive at the threshold)
+		
+		 
+		List<Object> results = service.getTestsOrdered(null, null, null, null, -1, 3);
+		
+		DataCounts map = new DataCounts(results);
+				
+		assertEquals("1", map.getValue("HEMOGLOBIN"));		
+		assertEquals("2", map.getValue("OTHER NON-CODED"));	
+		assertEquals("3", map.getValue("CD4 PANEL"));
+		assertNull(map.getValue("COMPLETE BLOOD COUNT"));
+	}
+	
+	@Test
+	@SkipBaseSetup
+	public void testOneCity() {
+		
+		System.out.println("Running Test: " + Thread.currentThread().getStackTrace()[1].getMethodName());
+		
+		// testing that uses only one city to get the Tests from just that city
+		
+		String cities = "Indianapolis";
+		
+		 
+		List<Object> results = service.getTestsOrdered(null, cities, null, null, -1, -1);
+		
+		DataCounts map = new DataCounts(results);
+				
+		assertEquals("1", map.getValue("COMPLETE BLOOD COUNT"));
+		assertNull(map.getValue("HEMOGLOBIN"));
+	}
+	
+	@Test
+	@SkipBaseSetup
+	public void testSomeCities() {
+		
+		System.out.println("Running Test: " + Thread.currentThread().getStackTrace()[1].getMethodName());
+		
+		// testing that uses two cities and gets the Tests of those cities
+		
+		String cities = "Indianapolis:Philadelphia";
+		
+		 
+		List<Object> results = service.getTestsOrdered(null, cities, null, null, -1, -1);
+		
+		DataCounts map = new DataCounts(results);
+		
+		assertEquals("1", map.getValue("HEMOGLOBIN"));		
+		assertEquals("2", map.getValue("OTHER NON-CODED"));	
+		assertEquals("3", map.getValue("CD4 PANEL"));
+		assertEquals("4", map.getValue("X-RAY CHEST"));
+		assertEquals("2", map.getValue("COMPLETE BLOOD COUNT"));
+
+	}
+	
+	
+	@Test
+	@SkipBaseSetup
+	public void testCityDoesNotExist() {
+		
+		System.out.println("Running Test: " + Thread.currentThread().getStackTrace()[1].getMethodName());
+		
+		// testing that a city that does not exist will not return results
+		
+		String cities = "Jeffersonville";
+		
+		 
+		List<Object> results = service.getTestsOrdered(null, cities, null, null, -1, -1);
+		
+		DataCounts map = new DataCounts(results);
+
+		assertNull(map.getValue("HEMOGLOBIN"));
+	}
+	
+	
+	@Test
+	@SkipBaseSetup
+	public void testAllCities() {
+		
+		System.out.println("Running Test: " + Thread.currentThread().getStackTrace()[1].getMethodName());
+		
+		// testing that it will do the same with a list of all the cities same as the null
+		
+		String cities = "Indianapolis:Philadelphia:New York:Chicago:Detroit:Los Angeles:San Diego";
+		
+		 
+		List<Object> results = service.getTestsOrdered(null, cities, null, null, -1, -1);
+		
+		DataCounts map = new DataCounts(results);
+				
+		assertEquals("1", map.getValue("HEMOGLOBIN"));		
+		assertEquals("2", map.getValue("OTHER NON-CODED"));	
+		assertEquals("3", map.getValue("CD4 PANEL"));
+		assertEquals("4", map.getValue("X-RAY CHEST"));
+		assertEquals("5", map.getValue("COMPLETE BLOOD COUNT"));
+
+	}
+	
+	@Test
+	@SkipBaseSetup
+	public void testBetweenDatesEverything() {
+		
+		System.out.println("Running Test: " + Thread.currentThread().getStackTrace()[1].getMethodName());
+		
+		// testing that it will do the same with a list of all the cities same as the null
+		
+		List<Object> results = service.getTestsOrdered(null, null, "2000-01-01", "2010-01-01", -1, -1);
+		
+		
+		
+		DataCounts map = new DataCounts(results);
+				
+		assertEquals("1", map.getValue("HEMOGLOBIN"));		
+		assertEquals("2", map.getValue("OTHER NON-CODED"));	
+		assertEquals("3", map.getValue("CD4 PANEL"));
+		assertEquals("4", map.getValue("X-RAY CHEST"));
+		assertEquals("5", map.getValue("COMPLETE BLOOD COUNT"));
+	
+	}
+
+	@Test
+	@SkipBaseSetup
+	public void testBetweenDatesNotIncludeEnds() {
+		
+		System.out.println("Running Test: " + Thread.currentThread().getStackTrace()[1].getMethodName());
+		
+		// testing that it will do the same with a list of all the cities same as the null
+				
+		
+		
+		List<Object> results = service.getTestsOrdered(null, null, "2008-08-03", "2008-08-18", -1, -1);
+		DataCounts map = new DataCounts(results);
+		
+		assertEquals("1", map.getValue("HEMOGLOBIN"));	
+		assertNull(map.getValue("OTHER NON-CODED"));	
+		assertEquals("3", map.getValue("CD4 PANEL"));
+		assertEquals("3", map.getValue("X-RAY CHEST"));
+		assertNull(map.getValue("COMPLETE BLOOD COUNT"));
+	
+	}
+
+	@Test
+	@SkipBaseSetup
+	public void testBetweenDatesNothing() {
+		
+		System.out.println("Running Test: " + Thread.currentThread().getStackTrace()[1].getMethodName());
+		
+		// testing that it will do the same with a list of all the cities same as the null
+				
+		
+		List<Object> results = service.getTestsOrdered(null, null, "2008-08-03", "2008-08-10", -1, -1);
+		
+		DataCounts map = new DataCounts(results);
+		
+		assertNull(map.getValue("COMPLETE BLOOD COUNT"));
+	
+	}
+
+	@Test
+	@SkipBaseSetup
+	public void testBetweenDatesIncludeStart() {
+		
+		System.out.println("Running Test: " + Thread.currentThread().getStackTrace()[1].getMethodName());
+		
+		// testing that it will do the same with a list of all the cities same as the null
+				
+		
+		List<Object> results = service.getTestsOrdered(null, null, "2008-08-01", "2008-08-18", -1, -1);
+		
+		DataCounts map = new DataCounts(results);
+				
+		assertEquals("1", map.getValue("HEMOGLOBIN"));		
+		assertEquals("2", map.getValue("OTHER NON-CODED"));	
+		assertEquals("3", map.getValue("CD4 PANEL"));
+		assertEquals("3", map.getValue("X-RAY CHEST"));
+		assertNull(map.getValue("COMPLETE BLOOD COUNT"));
+	
+	}
+
+	@Test
+	@SkipBaseSetup
+	public void testBetweenDatesIncludeEnd() {
+		
+		System.out.println("Running Test: " + Thread.currentThread().getStackTrace()[1].getMethodName());
+		
+		// testing that it will do the same with a list of all the cities same as the null
+				
+		
+		List<Object> results = service.getTestsOrdered(null, null, "2008-08-03", "2008-08-15", -1, -1);
+		
+		DataCounts map = new DataCounts(results);
+				
+		assertEquals("1", map.getValue("HEMOGLOBIN"));		
+		assertNull(map.getValue("OTHER NON-CODED"));	
+		assertEquals("3", map.getValue("CD4 PANEL"));
+		assertEquals("3", map.getValue("X-RAY CHEST"));
+		assertNull(map.getValue("COMPLETE BLOOD COUNT"));
+	
+	}
+
+	@Test
+	@SkipBaseSetup
+	public void testBetweenDatesIncludeBoth() {
+		
+		System.out.println("Running Test: " + Thread.currentThread().getStackTrace()[1].getMethodName());
+		
+		// testing that it will do the same with a list of all the cities same as the null
+				
+		
+		List<Object> results = service.getTestsOrdered(null, null, "2008-08-01", "2008-08-15", -1, -1);
+		
+		DataCounts map = new DataCounts(results);
+				
+		assertEquals("1", map.getValue("HEMOGLOBIN"));		
+		assertEquals("2", map.getValue("OTHER NON-CODED"));	
+		assertEquals("3", map.getValue("CD4 PANEL"));
+		assertEquals("3", map.getValue("X-RAY CHEST"));
+		assertNull(map.getValue("COMPLETE BLOOD COUNT"));
+	
+	}
+
+	@Test
+	@SkipBaseSetup
+	public void testStartDateBeforeEverything() {
+		
+		System.out.println("Running Test: " + Thread.currentThread().getStackTrace()[1].getMethodName());
+		
+		// testing that it will do the same with a list of all the cities same as the null
+				
+		
+		List<Object> results = service.getTestsOrdered(null, null, "2000-01-01", null, -1, -1);
+		
+		DataCounts map = new DataCounts(results);
+				
+		assertEquals("1", map.getValue("HEMOGLOBIN"));		
+		assertEquals("2", map.getValue("OTHER NON-CODED"));	
+		assertEquals("3", map.getValue("CD4 PANEL"));
+		assertEquals("4", map.getValue("X-RAY CHEST"));
+		assertEquals("5", map.getValue("COMPLETE BLOOD COUNT"));
+
+	}
+	
+	@Test
+	@SkipBaseSetup
+	public void testStartDateAfterEverything() {
+		
+		System.out.println("Running Test: " + Thread.currentThread().getStackTrace()[1].getMethodName());
+		
+		// testing that it will do the same with a list of all the cities same as the null
+				
+		
+		List<Object> results = service.getTestsOrdered(null, null, "2010-01-01", null, -1, -1);
+		
+		DataCounts map = new DataCounts(results);
+				
+		assertNull(map.getValue("HEMOGLOBIN"));		
+
+
+	}
+	
+	@Test
+	@SkipBaseSetup
+	public void testStartDateInTheMiddle() {
+		
+		System.out.println("Running Test: " + Thread.currentThread().getStackTrace()[1].getMethodName());
+		
+		// testing that it will do the same with a list of all the cities same as the null
+				
+		
+		List<Object> results = service.getTestsOrdered(null, null, "2008-08-16", null, -1, -1);
+		
+		DataCounts map = new DataCounts(results);
+				
+		assertEquals("1", map.getValue("X-RAY CHEST"));
+		assertEquals("5", map.getValue("COMPLETE BLOOD COUNT"));
+
+	}
+	
+	@Test
+	@SkipBaseSetup
+	public void testStartDateOnEnd() {
+		
+		System.out.println("Running Test: " + Thread.currentThread().getStackTrace()[1].getMethodName());
+		
+		// testing that it will do the same with a list of all the cities same as the null
+				
+		
+		List<Object> results = service.getTestsOrdered(null, null, "2008-08-15", null, -1, -1);
+		
+		DataCounts map = new DataCounts(results);
+				
+		assertEquals("1", map.getValue("HEMOGLOBIN"));	
+		assertNull(map.getValue("OTHER NON-CODED"));
+		assertEquals("3", map.getValue("CD4 PANEL"));
+		assertEquals("4", map.getValue("X-RAY CHEST"));
+		assertEquals("5", map.getValue("COMPLETE BLOOD COUNT"));
+	
+	}
+
+	@Test
+	@SkipBaseSetup
+	public void testEndDateEverything() {
+		
+		System.out.println("Running Test: " + Thread.currentThread().getStackTrace()[1].getMethodName());
+		
+		// testing that it will do the same with a list of all the cities same as the null
+				
+		
+		List<Object> results = service.getTestsOrdered(null, null, null, "2010-01-01", -1, -1);
+		
+		DataCounts map = new DataCounts(results);
+				
+		assertEquals("1", map.getValue("HEMOGLOBIN"));		
+		assertEquals("2", map.getValue("OTHER NON-CODED"));	
+		assertEquals("3", map.getValue("CD4 PANEL"));
+		assertEquals("4", map.getValue("X-RAY CHEST"));
+		assertEquals("5", map.getValue("COMPLETE BLOOD COUNT"));
+
+	}
+	
+	@Test
+	@SkipBaseSetup
+	public void testEndDateBeforeEverything() {
+		
+		System.out.println("Running Test: " + Thread.currentThread().getStackTrace()[1].getMethodName());
+		
+		// testing that it will do the same with a list of all the cities same as the null
+				
+		
+		List<Object> results = service.getTestsOrdered(null, null, null, "2000-01-01", -1, -1);
+		
+		DataCounts map = new DataCounts(results);
+				
+		assertNull(map.getValue("HEMOGLOBIN"));
+
+	}
+	
+	@Test
+	@SkipBaseSetup
+	public void testEndDateInTheMiddle() {
+		
+		System.out.println("Running Test: " + Thread.currentThread().getStackTrace()[1].getMethodName());
+		
+		// testing that it will do the same with a list of all the cities same as the null
+				
+		
+		List<Object> results = service.getTestsOrdered(null, null, null, "2008-08-16", -1, -1);
+		
+		DataCounts map = new DataCounts(results);
+				
+		assertEquals("1", map.getValue("HEMOGLOBIN"));		
+		assertEquals("2", map.getValue("OTHER NON-CODED"));	
+		assertEquals("3", map.getValue("CD4 PANEL"));
+		assertEquals("3", map.getValue("X-RAY CHEST"));
+		assertNull(map.getValue("COMPLETE BLOOD COUNT"));
+
+	}
+	
+	@Test
+	@SkipBaseSetup
+	public void testEndDateOnEnd() {
+		
+		System.out.println("Running Test: " + Thread.currentThread().getStackTrace()[1].getMethodName());
+		
+		// testing that it will do the same with a list of all the cities same as the null
+				
+		
+		List<Object> results = service.getTestsOrdered(null, null, null, "2008-08-15", -1, -1);
+		
+		DataCounts map = new DataCounts(results);
+				
+		assertEquals("1", map.getValue("HEMOGLOBIN"));		
+		assertEquals("2", map.getValue("OTHER NON-CODED"));	
+		assertEquals("3", map.getValue("CD4 PANEL"));
+		assertEquals("3", map.getValue("X-RAY CHEST"));
+		assertNull(map.getValue("COMPLETE BLOOD COUNT"));
+
+	}
+}
